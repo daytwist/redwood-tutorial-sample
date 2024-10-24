1cb925d5e15bfd6196ce127f19e2b58df7e0e290
Natsumi Oishi <98074753+daytwist@users.noreply.github.com>
2024-10-24 14:23:58 +0900

    不要なコメント削除

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,11 +1,5 @@
 import { comments, createComment } from './comments'
 
-// Generated boilerplate tests do not account for all circumstances
-// and can fail without adjustments, e.g. Float.
-//           Please refer to the RedwoodJS Testing Docs:
-//       https://redwoodjs.com/docs/testing#testing-services
-// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
-
 describe('comments', () => {
   scenario('returns all comments', async (scenario) => {
     const result = await comments()

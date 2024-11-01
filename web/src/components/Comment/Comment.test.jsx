07dc70bd69da522eb4555c67f10c3b0264445e90
Natsumi Oishi <98074753+daytwist@users.noreply.github.com>
2024-11-01 10:25:53 +0900

    コメント削除ボタンの表示テスト追加

--- web/src/components/Comment/Comment.test.jsx
+++ web/src/components/Comment/Comment.test.jsx
@@ -1,21 +1,41 @@
-import { render, screen } from '@redwoodjs/testing'
+import { render, screen, waitFor } from '@redwoodjs/testing'
 
 import Comment from './Comment'
 
+const COMMENT = {
+  name: 'John Doe',
+  body: 'This is my comment',
+  createdAt: '2024-10-20T07:28:00Z',
+}
+
 describe('Comment', () => {
   it('renders successfully', () => {
-    const comment = {
-      name: 'John Doe',
-      body: 'This is my comment',
-      createdAt: '2024-10-20T07:28:00Z',
-    }
-    render(<Comment comment={comment} />)
+    render(<Comment comment={COMMENT} />)
 
-    expect(screen.getByText(comment.name)).toBeInTheDocument()
-    expect(screen.getByText(comment.body)).toBeInTheDocument()
+    expect(screen.getByText(COMMENT.name)).toBeInTheDocument()
+    expect(screen.getByText(COMMENT.body)).toBeInTheDocument()
     const dateExpect = screen.getByText('20 10月 2024')
     expect(dateExpect).toBeInTheDocument()
     expect(dateExpect.nodeName).toEqual('TIME')
-    expect(dateExpect).toHaveAttribute('datetime', comment.createdAt)
+    expect(dateExpect).toHaveAttribute('datetime', COMMENT.createdAt)
+  })
+
+  it('does not render a delete button if user is logged out', async () => {
+    render(<Comment comment={COMMENT} />)
+
+    await waitFor(() =>
+      expect(screen.queryByText('Delete')).not.toBeInTheDocument()
+    )
+  })
+
+  it('renders a delete button if the user is a moderator', async () => {
+    mockCurrentUser({
+      id: 1,
+      email: 'moderator@moderator.com',
+      roles: 'moderator',
+    })
+    render(<Comment comment={COMMENT} />)
+
+    await waitFor(() => expect(screen.getByText('Delete')).toBeInTheDocument())
   })
 })

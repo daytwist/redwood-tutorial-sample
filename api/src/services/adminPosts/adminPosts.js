395eb86e322e3efeafe9d4aa7e3a349e53ef821f
Natsumi Oishi <98074753+daytwist@users.noreply.github.com>
2024-11-11 16:13:51 +0900

    編集、削除のアクセス権を制限

--- api/src/services/adminPosts/adminPosts.js
+++ api/src/services/adminPosts/adminPosts.js
@@ -1,4 +1,14 @@
+import { ForbiddenError } from '@redwoodjs/graphql-server'
+
 import { db } from 'src/lib/db'
+
+const verifyOwnership = async ({ id }) => {
+  if (await adminPost({ id })) {
+    return true
+  } else {
+    throw new ForbiddenError("You don't have access to this post")
+  }
+}
 
 export const adminPosts = () => {
   return db.post.findMany({ where: { userId: context.currentUser.id } })
@@ -16,7 +26,9 @@ export const createPost = ({ input }) => {
   })
 }
 
-export const updatePost = ({ id, input }) => {
+export const updatePost = async ({ id, input }) => {
+  await verifyOwnership({ id })
+
   return db.post.update({
     data: input,
     where: { id },
